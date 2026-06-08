a9bad88fe6077dc24015946d3709d6642e22b575
JordynViviers <jdviviers2020@gmail.com>
2026-06-08 10:51:06 -0600

    Update page.tsx

--- frontend/app/(public)/page.tsx
+++ frontend/app/(public)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <section
         className="relative min-h-screen flex items-center justify-center px-6 overflow-hidden bg-cover bg-center"
         style={{
-          backgroundImage: "url('/calgary-bg.jpg')",
+          backgroundImage: "url(/calgary-bg.avif)",
         }}
       >
         {/* Translucent Overlay */}
