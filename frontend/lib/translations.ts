4e6707175b4c6f39c99b2e93b0dbad7b29975089
JordynViviers <jdviviers2020@gmail.com>
2026-06-09 10:58:58 -0600

    Create translations.ts

---
+++ frontend/lib/translations.ts
@@ -0,0 +1,24 @@
+export const translations = {
+
+  English: {
+    home: "Home",
+    technologies: "Technologies",
+    events: "Events",
+    apply: "Apply",
+  },
+
+  "Français canadien": {
+    home: "Accueil",
+    technologies: "Technologies",
+    events: "Événements",
+    apply: "Postuler",
+  },
+
+  "Español latinoamericano": {
+    home: "Inicio",
+    technologies: "Tecnologías",
+    events: "Eventos",
+    apply: "Aplicar",
+  },
+
+};
