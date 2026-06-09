75e5f2bcb349b92ba101f220d5ca7a21ba184c51
JordynViviers <jdviviers2020@gmail.com>
2026-06-09 11:00:31 -0600

    Add LanguageProvider to PublicLayout
    Wrap the layout in a LanguageProvider component to manage language context.

--- frontend/app/(public)/layout.tsx
+++ frontend/app/(public)/layout.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
 import LanguageSelector from "@/components/LanguageSelector";
+import { LanguageProvider }
+from "@/components/LanguageProvider";
 
 export default function PublicLayout({
   children,
@@ -7,7 +9,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <LanguageProvider>
       <Navbar />
 
       <div className="flex justify-end px-8 py-3 bg-white">
@@ -15,7 +17,7 @@ export default function PublicLayout({
       </div>
 
       {children}
-    </>
+    <LanguageProvider />
   );
 }
 
