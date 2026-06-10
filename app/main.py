f20ab618a299b72599b90e94fd2b5015d0378ebe
JordynViviers <jdviviers2020@gmail.com>
2026-06-10 09:39:55 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -50,7 +50,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://calgary-compass-self.vercel.app",
+    ]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
