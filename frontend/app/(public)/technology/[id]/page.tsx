19940eb7f0d6c88fb626daf3d32a4c06ab676772
JordynViviers <jdviviers2020@gmail.com>
2026-06-24 10:18:10 -0600

    Update page.tsx

--- frontend/app/(public)/technology/[id]/page.tsx
+++ frontend/app/(public)/technology/[id]/page.tsx
@@ -651,7 +651,7 @@ const weightedAverage =
 
       {/* ASSESSMENTS */}
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 md:gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-16">
         
 
         {/* AI ASSESSMENT */}
