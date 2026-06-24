1aa918fbcedfd5ce6a257fd0fcfbf9f19e2c180b
JordynViviers <jdviviers2020@gmail.com>
2026-06-24 11:21:44 -0600

    Create loading.tsx

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,9 @@
+import CompassLoader from "@/components/CompassLoader";
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <CompassLoader />
+    </div>
+  );
+}
